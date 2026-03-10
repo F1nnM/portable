1e81475d974497494d4c683988491050b9f02fb4
Finn Melzer <finn.melzer@mayflower.de>
2026-03-10 22:10:46 +0100

    Disabled dev tools for scaffold

--- scaffolds/nuxt-postgres/nuxt.config.ts
+++ scaffolds/nuxt-postgres/nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-03-01",
   ssr: true,
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   devServer: {
     host: "0.0.0.0",
   },
